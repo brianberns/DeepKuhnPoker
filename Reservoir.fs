ee70db616aa9c0dfcc4ceef3b74113e40101bd3a
Brian Berns <brianberns@gmail.com>
2024-10-05 15:58:02 -0400

    Reservoir sampling.

--- Reservoir.fs
+++ Reservoir.fs
@@ -26,3 +26,12 @@ module Reservoir =
                 reservoir.Random.Next(reservoir.Items.Count)
         { reservoir with
             Items = Map.add idx item reservoir.Items }
+
+    let trySample numSamples reservoir =
+        let count = reservoir.Items.Count
+        if numSamples >= count then
+            let indexes = [| 0 .. count-1 |]
+            reservoir.Random.Shuffle(indexes)
+            Seq.init numSamples (fun i -> indexes[i])
+                |> Some
+        else None
