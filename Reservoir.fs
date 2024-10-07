68bb618e9736ca7d213b761e10667f2d95a1b31b
Brian Berns <brianberns@gmail.com>
2024-10-07 13:06:22 -0400

    Polish.

--- Reservoir.fs
+++ Reservoir.fs
@@ -2,15 +2,27 @@
 
 open System
 
+/// Reservoir sampler.
+/// https://en.wikipedia.org/wiki/Reservoir_sampling
 type Reservoir<'t> =
     {
+        /// Random number generator.
         Random : Random
+
+        /// Capacity of this reservoir.
         Capacity : int
+
+        /// Items stored in this reservoir, indexed from
+        /// 0 to Capacity - 1.
         Items : Map<int, 't>
     }
 
+    /// Number of items stored in this reservoir.
+    member this.Count = this.Items.Count
+
 module Reservoir =
 
+    /// Creates an empty reservoir.
     let create rng capacity =
         {
             Random = rng
@@ -18,21 +30,24 @@ module Reservoir =
             Items = Map.empty
         }
 
-    let add item reservoir =
+    /// Adds the given item to the given reservior, replacing
+    /// an existing item at random if necessary.
+    let add item (reservoir : Reservoir<_>) =
         let idx =
-            if reservoir.Items.Count < reservoir.Capacity then
-                reservoir.Items.Count
+            if reservoir.Count < reservoir.Capacity then
+                reservoir.Count
             else
-                reservoir.Random.Next(reservoir.Items.Count)
+                reservoir.Random.Next(reservoir.Count)
         { reservoir with
             Items = Map.add idx item reservoir.Items }
 
-    let trySample numSamples reservoir =
-        let count = reservoir.Items.Count
-        if numSamples <= count then
-            let indexes = [| 0 .. count-1 |]
-            reservoir.Random.Shuffle(indexes)
+    /// Answers the given number of items from the given
+    /// reservoir at random, if possible.
+    let trySample numSamples (reservoir : Reservoir<_>) =
+        if numSamples <= reservoir.Count then
+            let idxs = [| 0 .. reservoir.Count - 1 |]
+            reservoir.Random.Shuffle(idxs)
             Seq.init numSamples (fun i ->
-                reservoir.Items[indexes[i]])
+                reservoir.Items[idxs[i]])
                 |> Some
         else None
