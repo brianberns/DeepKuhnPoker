afebfabaa0d2cfaec4c36be80e0632a02408f305
Brian Berns <brianberns@gmail.com>
2024-10-07 15:12:53 -0400

    Modularized.

--- KuhnPoker.fs
+++ KuhnPoker.fs
@@ -47,3 +47,41 @@ module KuhnPoker =
 
             // game not over
         | _ -> None
+
+    module Encoding =
+
+        /// Length of a one-hot vector.
+        let private oneHotLength =
+            max
+                actions.Length   // 2
+                deck.Length      // 3
+
+        /// Length of longest info set key. E.g. "Jcb".
+        let private maxInfoSetKeyLength = 3
+
+        let encodedLength = maxInfoSetKeyLength * oneHotLength
+
+        /// Encodes the given info set key as a vector.
+        let encodeInput (infoSetKey : string) =
+
+            let toOneHot c =
+                let oneHot =
+                    match c with
+                        | 'J' -> [| 1.0f; 0.0f; 0.0f |]
+                        | 'K' -> [| 0.0f; 1.0f; 0.0f |]
+                        | 'Q' -> [| 0.0f; 0.0f; 1.0f |]
+                        | 'b' -> [| 1.0f; 0.0f; 0.0f |]
+                        | 'c' -> [| 0.0f; 1.0f; 0.0f |]
+                        | _ -> failwith "Unexpected"
+                assert(oneHot.Length = oneHotLength)
+                oneHot
+
+            let encoded =
+                [|
+                    for c in infoSetKey do
+                        yield! toOneHot c
+                    yield! Array.zeroCreate
+                        (oneHotLength * (maxInfoSetKeyLength - infoSetKey.Length))
+                |]
+            assert(encoded.Length = encodedLength)
+            encoded
--- Network.fs
+++ Network.fs
@@ -23,17 +23,8 @@ type StrategySample =
 
 module Network =
 
-    /// Length of longest info set key. E.g. "Jcb".
-    let private maxInfoSetKeyLength = 3
-
-    /// Length of a one-hot vector.
-    let private oneHotLength =
-        max
-            KuhnPoker.actions.Length   // 2
-            KuhnPoker.deck.Length      // 3
-
     /// Length of neural network input.
-    let private inputSize = maxInfoSetKeyLength * oneHotLength
+    let private inputSize = KuhnPoker.Encoding.encodedLength
 
     /// Length of neural network output.
     let private outputSize = KuhnPoker.actions.Length
@@ -53,35 +44,10 @@ module Network =
             Linear(hiddenSize, outputSize),
             Softmax(dim = 1))
 
-    /// Encodes the given info set key as a vector.
-    let private encodeInput (infoSetKey : string) =
-
-        let toOneHot c =
-            let oneHot =
-                match c with
-                    | 'J' -> [| 1.0f; 0.0f; 0.0f |]
-                    | 'K' -> [| 0.0f; 1.0f; 0.0f |]
-                    | 'Q' -> [| 0.0f; 0.0f; 1.0f |]
-                    | 'b' -> [| 1.0f; 0.0f; 0.0f |]
-                    | 'c' -> [| 0.0f; 1.0f; 0.0f |]
-                    | _ -> failwith "Unexpected"
-            assert(oneHot.Length = oneHotLength)
-            oneHot
-
-        let encoded =
-            [|
-                for c in infoSetKey do
-                    yield! toOneHot c
-                yield! Array.zeroCreate
-                    (oneHotLength * (maxInfoSetKeyLength - infoSetKey.Length))
-            |]
-        assert(encoded.Length = inputSize)
-        encoded
-
     /// Gets the advantage for the given info set.
     let getAdvantage infoSetKey (advantageNetwork : Network) =
         (infoSetKey
-            |> encodeInput
+            |> KuhnPoker.Encoding.encodeInput
             |> torch.tensor)
             --> advantageNetwork
 
@@ -97,7 +63,7 @@ module Network =
                 samples
                     |> Seq.map (fun (sample : AdvantageSample) ->
                         sample.InfoSetKey
-                            |> encodeInput)
+                            |> KuhnPoker.Encoding.encodeInput)
                     |> array2D
                     |> torch.tensor
             let targets =
