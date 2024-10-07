279159cf4a3824a9b44e996b32d4fd0a3c652df6
Brian Berns <brianberns@gmail.com>
2024-10-07 13:16:26 -0400

    Modularized.

---
+++ InformationSet.fs
@@ -0,0 +1,28 @@
+﻿namespace DeepKuhnPoker
+
+open MathNet.Numerics.LinearAlgebra
+
+module InformationSet =
+
+    /// Uniform strategy: All actions have equal probability.
+    let private uniformStrategy =
+        DenseVector.create
+            KuhnPoker.actions.Length
+            (1.0f / float32 KuhnPoker.actions.Length)
+
+    /// Normalizes a strategy such that its elements sum to
+    /// 1.0 (to represent action probabilities).
+    let private normalize strategy =
+
+            // assume no negative values during normalization
+        assert(Vector.forall (fun x -> x >= 0.0f) strategy)
+
+        let sum = Vector.sum strategy
+        if sum > 0.0f then strategy / sum
+        else uniformStrategy
+
+    /// Computes regret-matching strategy from given regrets.
+    let getStrategy regrets =
+        regrets
+            |> Vector.map (max 0.0f)   // clamp negative regrets
+            |> normalize
--- Program.fs
+++ Program.fs
@@ -7,31 +7,6 @@ open MathNet.Numerics.LinearAlgebra
 
 open TorchSharp
 
-module InformationSet =
-
-    /// Uniform strategy: All actions have equal probability.
-    let private uniformStrategy =
-        DenseVector.create
-            KuhnPoker.actions.Length
-            (1.0f / float32 KuhnPoker.actions.Length)
-
-    /// Normalizes a strategy such that its elements sum to
-    /// 1.0 (to represent action probabilities).
-    let private normalize strategy =
-
-            // assume no negative values during normalization
-        assert(Vector.forall (fun x -> x >= 0.0f) strategy)
-
-        let sum = Vector.sum strategy
-        if sum > 0.0f then strategy / sum
-        else uniformStrategy
-
-    /// Computes regret-matching strategy from given regrets.
-    let getStrategy regrets =
-        regrets
-            |> Vector.map (max 0.0f)   // clamp negative regrets
-            |> normalize
-
 module KuhnCfrTrainer =
 
     /// Random number generator.
